c79b2a505f1d97d6518a36af618f072a66d50c93
johnny-psh <psh.johnny@gmail.com>
2024-05-25 22:23:50 +0800

    authIssues

--- angelhack-fontend/src/pages/_app.tsx
+++ angelhack-fontend/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import NavBar from "@/components/NavBar";
 import "@/styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 import type { AppProps } from "next/app";
+import { AuthProvider } from '../context/AuthContext';
 import React from "react";
 
 interface PageConfigProps {
@@ -18,10 +19,13 @@ const PageConfig: React.FC<PageConfigProps> = ({ children }) => {
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    
     <NextUIProvider>
       <NavBar />
       <PageConfig>
+      <AuthProvider>
         <Component {...pageProps} />
+      </AuthProvider>
       </PageConfig>
     </NextUIProvider>
   );
